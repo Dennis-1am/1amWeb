ec0e26de25debcb9f6f92befe1210722c7438944
Dennis lam <dennis@a0-78-17-a4-70-7d.wireless.temple.edu>
2023-01-17 12:55:13 -0500

    base layout

--- js/script.js
+++ js/script.js
@@ -1,104 +1,41 @@
-window.onload = function () {
-    printMsg("Hello, I'm a non-intellegient assistance bot. I'm here to help you learn more about Dennis. Would you like to learn more about Dennis?");
-    loadOptions(["Yes", "No"]);
-}
+let expblock = document.getElementsByClassName("exp-block");
+let summary = document.getElementsByClassName("Summary");
+let skills = document.getElementsByClassName("Skills");
 
-document.addEventListener("mousemove", function (event) {
+let exptitles = ["Relay Networks", "Lehigh University", "Signet Jewelers"];
 
-    // get the mouse position on the screen
-    let mouseX = event.clientX;
-    let mouseY = event.clientY;
-
-    let pupil = document.getElementById("pupil");
-    // get the position of the pupil
-    let pupilX = pupil.offsetLeft;
-    let pupilY = pupil.offsetTop;
-
-    // calculate the difference between the mouse position and the pupil position
-    let diffX = (mouseX - pupilX) * .1; // multiply by .1 to slow down the movement
-    let diffY = (mouseY - pupilY) * .1;
-
-    // limit the movement of the pupil to within the eye
-    if (diffX > 150) {
-        diffX = 150;
-    } else if (diffX < -150) {
-        diffX = -150;
-    }
-
-    if (diffY > 30) {
-        diffY = 30;
-    } else if (diffY < -30) {
-        diffY = -30;
-    }
-
-    pupil.style.transform = "translate(" + diffX + "px, " + diffY + "px)";
-
-});
-
-document.getElementById("msgbox").addEventListener("mouseover", function () {
-    changeColor();
-    document.getElementById("msgbox").addEventListener("mouseout", function () {
-        resetColor();
+for(let i = 0; i < expblock.length; i++) {
+    // styling
+    let experience = expblock[i]
+    experience.style.background = "blue";
+    experience.style.color = "white";
+    experience.style.padding = "10px";
+    experience.style.borderRadius = "20px";
+    experience.style.border = "10px solid white";
+    experience.style.boxShadow = "0px 5px 5px #888888";
+    // event listener for mouseover and mouseout
+    experience.addEventListener("mouseover", function() {
+        experience.style.background = "white";
+        experience.style.color = "black";1
     });
-});
 
-document.getElementsByClassName("options")[0].addEventListener("mouseover", function () {
-    changeColor();
-    document.getElementsByClassName("options")[0].addEventListener("mouseout", function () {
-        resetColor();
-    });
-});
-
-document.getElementsByClassName("options")[1].addEventListener("mouseover", function () {
-    changeColor();
-    document.getElementsByClassName("options")[1].addEventListener("mouseout", function () {
-        resetColor();
-    });
-});
-
-function changeColor() {
-    let pupil = document.getElementById("pupil");
-    let iris = document.getElementById("iris");
-    pupil.style.backgroundColor = "red";
-    pupil.style.padding = "2rem";
-    iris.style.padding = "1rem";
-}
-
-function resetColor() {
-    let pupil = document.getElementById("pupil");
-    let iris = document.getElementById("iris");
-    pupil.style.backgroundColor = "black";
-    pupil.style.padding = "3rem";
-    iris.style.padding = "1rem";
-}
-
-function printMsg(msg) {
-    let msgbox = document.getElementById("msgbox");
-    let i = 0;
-    let timer = setInterval(function () {
-        if (i < msg.length) {
-            msgbox.innerHTML += msg.charAt(i);
-            i++;
-        } else {
-            clearInterval(timer);
+    experience.addEventListener("mouseleave", function() {
+        experience.style.background = "blue";
+        experience.style.color = "white";
+        for(let j = 0; j < exptitles.length; j++) {
+            summary[j].style.display = "none";
+            skills[j].style.display = "none";
         }
-    }, 35);
-}
+    });
 
-function loadOptions(textList){ // textList is an array of strings in this case yes and no
-    let optionlist = document.getElementsByClassName("options");
-    let i = 0; // index of the optionList
-    let j = 0; // index of the word
-    let timer = setInterval(function () {
-        if(i < optionlist.length && j < textList[i].length){
-            optionlist[i].innerHTML += textList[i][j];
-            j++;
-        } else if(i < optionlist.length && j >= textList[i].length){
-            i++;
-            j = 0;
-        } else {
-            clearInterval(timer);
+    // event listener for click
+    experience.addEventListener("click", function() {
+        let title = experience.children[0].innerHTML;
+        for(let j = 0; j < exptitles.length; j++) {
+            if(title == exptitles[j]) {
+                summary[j].style.display = "block";
+                skills[j].style.display = "block";
+            }
         }
-    }
-    , 100);
+    });
 }
