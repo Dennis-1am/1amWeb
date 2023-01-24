94aad8bdd77ea810fd2d179def0850b4d8c68238
Dennis lam <dennis@dyn218241.cc.lehigh.edu>
2023-01-24 00:23:00 -0500

    sidebar

--- js/script.js
+++ js/script.js
@@ -9,14 +9,15 @@ for(let i = 0; i < expblock.length; i++) {
     let experience = expblock[i]
     experience.style.background = "blue";
     experience.style.color = "white";
-    experience.style.padding = "10px";
-    experience.style.borderRadius = "20px";
-    experience.style.border = "10px solid white";
-    experience.style.boxShadow = "0px 5px 5px #888888";
+    experience.style.padding = "5px";
+    experience.style.borderRadius = "15px";
+    experience.style.border = "5px solid black";
+    experience.style.boxShadow = "20px 38px 34px -26px #888888";
     // event listener for mouseover and mouseout
     experience.addEventListener("mouseover", function() {
         experience.style.background = "white";
-        experience.style.color = "black";1
+        experience.style.color = "black";
+        experience.style.boxShadow = "2px 8px 4px -6px #888888";
     });
 
     experience.addEventListener("mouseleave", function() {
@@ -25,6 +26,7 @@ for(let i = 0; i < expblock.length; i++) {
         for(let j = 0; j < exptitles.length; j++) {
             summary[j].style.display = "none";
             skills[j].style.display = "none";
+            experience.style.boxShadow = "20px 38px 34px -26px #888888";
         }
     });
 
@@ -39,3 +41,21 @@ for(let i = 0; i < expblock.length; i++) {
         }
     });
 }
+
+let navblock = document.getElementsByClassName("nav-block");
+for(let i = 0; i < navblock.length; i++){
+    navblock[i].addEventListener("mouseover", function() {
+        navblock[i].style.color = "blue";
+    });
+
+    navblock[i].addEventListener("mouseleave", function() {
+        navblock[i].style.color = "black";
+    } );
+}
+
+// track mouse position on page and display in console
+document.addEventListener("mousemove", function(event) {
+    
+});
+
+
